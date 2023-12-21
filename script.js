700f35f92f13769dadbe06be8dfb0e927a34eab8
astrosuka <yegresvoselok@gmail.com>
2023-12-21 16:54:33 -0300

    Create centered 16x16 grid of square divs

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let container = document.querySelector('#container');
 
 for (let i = 0; i < (16 * 16); i++) {
     let div = document.createElement('div');
-    div.textContent = 'I AM A DIV';
-    div.style.border = 'black solid 1px'
+    // div.textContent = i;
+    div.classList.add('item');
     container.appendChild(div);
 };
